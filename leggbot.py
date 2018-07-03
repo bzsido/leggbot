7dc8e938370cec21273bb59de60acbce948a46d1
Balazs Zsido <balazs.zsido@gmail.com>
2018-07-03 06:51:28 +0200

    getting the code ready to be deployed with up

--- leggbot.py
+++ leggbot.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 from random import choice
 
 # initial setup
@@ -19,15 +20,26 @@ def legg(bot, update):
     item = choice(items)
     bot.send_message(chat_id=update.message.chat_id, text=item)
 
+# not working yet!
+def leggs(bot, update):
+    query = update.inline_query.query
+    if not query:
+        return
+    items = ['legg', 'mimph', 'AAAA', 'murph', 'fibsh', 'liggk']
+    results = choice(items)
+    bot.answer_inline_query(update.inline_query.id, results)
+
 def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand. Mimph!")
 
 start_handler = CommandHandler('start', start)
 legg_handler = MessageHandler(Filters.text, legg)
+leggs_handler = InlineQueryHandler(leggs)
 unknown_handler = MessageHandler(Filters.command, unknown)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(legg_handler)
+dispatcher.add_handler(leggs_handler)
 dispatcher.add_handler(unknown_handler)
 
 updater.start_polling()
